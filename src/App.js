b09d63d7e1ca86d27709800098f39d77a164e063
Shuvendu Samanta <69745389+ShuvenduSamanta@users.noreply.github.com>
2023-04-23 12:17:19 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Selfintro from './components/Selfintro';
+import Roadmap from './components/Roadmap';
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      
       <Navbar />
       <Selfintro />
+      
+
+      
     </div>
   );
 }
--- src/components/Selfintro.js
+++ src/components/Selfintro.js
@@ -1,15 +1,41 @@
 import React from 'react'
+import Roadmap from './Roadmap'
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import { FaFacebookF, FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import { Link as log } from 'react-router-dom';
 import '../App.css'
 const Selfintro = () => {
   return (
+    
     <section className='intro'>
+      
         <div className='intro-content'>
             <h1>I am <br></br>Shuvendu Samanta</h1>
-            <h3>Crypto Lover</h3><br></br><br></br>
+            <h3>Crypto <span>Lover</span></h3><br></br><br></br>
             <p>I am a blockchain expert with 1 years of experience in the field. I have a deep understanding of blockchain technology and its various applications, including smart contracts, decentralized finance, and cryptocurrency. I have worked on several blockchain projects, both as a developer and a consultant, and I am passionate about the transformative potential of this technology. I look forward to sharing my knowledge and expertise with others who are interested in blockchain.</p>
             <div className='btn-group'>
-                button
+            
+              <a href='/roadmap'>click</a>
+              <log to="/roadmap">
+                Login
+                
+              </log>
+              <Router>
+                <Routes>
+                  <Route path="/roadmap" element={ <Roadmap/> } />
+                </Routes>
+              </Router>
+              
+          
             </div>
+        </div>
+        <div className="social">
+          <a href="http://"><FaFacebookF /></a>
+          <a href="http://"><FaTwitter /></a>
+          <a href="http://"><FaInstagram /></a>
+          <a href="http://"><SiLeetcode /></a>
+          <a href="http://"><FaGithub /></a>
         </div>
 
     </section>
